15ce18495f741bd05ac731621a6b66c71dd4658d
Mads Collin <madsc@hey.com>
2021-11-11 14:11:44 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [`_site/**/*.html`],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        teal: colors.teal,
+      },
+      gridTemplateRows: {
+        '[auto,auto,1fr]': 'auto auto 1fr',
+      },
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+  ],
 };
